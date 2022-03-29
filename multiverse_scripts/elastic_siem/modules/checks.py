a6fa0bfab19f02cf3b1750aed202d2bad3067a16
John Torakis <jtorakis@skroutz.gr>
2022-03-29 19:25:50 +0300

    Add toml, fix yaml

--- multiverse_scripts/elastic_siem/modules/checks.py
+++ multiverse_scripts/elastic_siem/modules/checks.py
@@ -1,12 +1,22 @@
 import json
 import sys
 
+def check_toml(data):
+    try:
+        import toml
+        return toml.loads(data)
+    except ImportError:
+        print("TOML not supported - use: 'pip install toml'")
+    return {}
+
 def check_yaml(data):
     try:
         import yaml
-        return yaml.load(data)
+        return yaml.safe_load(data)
     except ImportError:
         print("YAML not supported - use: 'pip install PyYAML'")
+    except yaml.parser.ParserError:
+        return {}
     return {}
 
 def check_json(data):
